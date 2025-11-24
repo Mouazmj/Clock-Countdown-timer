4a44e1769f3fc2f262e2d6b4b924fd51929963e2
Mouazmj <mouazjamal05@gmail.com>
2025-11-24 22:10:39 +0100

    Adding the warning p to the js to check and send error if one or more inputs are empty, and return to stom the form

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.eventForm')
+const warning = document.getElementById('warning')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -7,5 +8,8 @@ const eventName = document.querySelector('.event-name')
 const eventDate = document.querySelector('.date')
 const eventTime = document.querySelector('.time')
 
-
+if (eventName.value === '' || eventDate.value === '' || eventTime.value === '') {
+    warning.textContent = 'Please fill all the inputs';
+    return; 
+}
 })
