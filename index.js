644afbca6065d1e533b3c2aee3f69ed5cc06ef9e
Mouazmj <mouazjamal05@gmail.com>
2025-11-24 22:06:06 +0100

    Adding waring p

--- index.js
+++ index.js
@@ -7,7 +7,5 @@ const eventName = document.querySelector('.event-name')
 const eventDate = document.querySelector('.date')
 const eventTime = document.querySelector('.time')
 
-console.log(eventName.value)
-console.log(eventDate.value)
-console.log(eventTime.value)
+
 })
